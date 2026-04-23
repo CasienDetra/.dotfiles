4a7b0c6c32c755b4efeecaee8ef7b7646f32afad
CasienDetra <sarath.rithyyanouk.1223@rupp.edu.kh>
2026-04-23 19:41:32 +0700

    feat(script): clear arch, disk , network and slow service

---
+++ script/slow_service_to_start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+LOG_FILE="/var/log/boot_time.log"
+
+echo "🕰  Analyze system startup time" | tee -a $LOG_FILE
+
+BOOT_TIME=$(systemd-analyze)
+
+BLAME=$(systemd-analyze blame | head -n 10)
+
+{
+	echo "⏱️ $(date)"
+	echo "$BOOT_TIME"
+	echo "📌 Ten most slowest services:"
+	echo "$BLAME"
+	echo "-----------------------------"
+} >>$LOG_FILE
+
+echo "✅ Done ." | tee -a $LOG_FILE
